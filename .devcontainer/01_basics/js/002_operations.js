97664206566c8408a88926a49aac33eb63fc8196
kuldeepvashisth <vashisthdeep829@gmail.com>
2025-11-06 17:48:02 +0000

    operations

---
+++ .devcontainer/01_basics/js/002_operations.js
@@ -0,0 +1,14 @@
+let value=3
+let negvalue=-value
+//we can do math operation like as in cout
+console.log(2**3) //means 2^3
+
+let str1="hello"
+let str2=" kv"
+let str3=str1+str2
+console.log(str3)
+
+console.log("1"+2+"2")//=>122
+console.log(1+"2"+"2")//=>122
+console.log(1+2+"2")//=>(1+2)=>32
+

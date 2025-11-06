a2cef2a80f6f2ab3f1dfdc4fc77800c43e608a73
kuldeepvashisth <vashisthdeep829@gmail.com>
2025-11-06 17:20:25 +0000

    data type inter conversion

---
+++ .devcontainer/01_basics/js/001_datatype_conversion.js
@@ -0,0 +1,20 @@
+let score="33"
+
+console.log(typeof score); //output;-string
+
+// jis bhi datatype mai krna hai uska first letter capitall ho
+let valueinno=Number(score)
+//Nan(also a type means-> not a number)
+console.log(typeof valueinno);
+//output;-number
+/*conversion {string ->number}
+"33"=>33
+"33abc"=>nan
+"true/false"=>1/0
+
+similarly we can do to :
+Boolean{
+"kv"=>true
+}
+
+*/
